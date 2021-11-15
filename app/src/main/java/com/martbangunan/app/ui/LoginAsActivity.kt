fb3970d1d90df79d342015a92ca33de0961d4b26
Maulidani <maulidanimahmud@gmail.com>
2021-11-16 00:32:55 +0800

    update

---
+++ app/src/main/java/com/martbangunan/app/ui/LoginAsActivity.kt
@@ -0,0 +1,12 @@
+package com.martbangunan.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.martbangunan.app.R
+
+class LoginAsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_as)
+    }
+}
