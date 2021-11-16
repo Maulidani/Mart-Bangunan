4ac65cd5a4e55b772ac5dc386132aba397ea86f8
Maulidani <maulidanimahmud@gmail.com>
2021-11-16 17:11:31 +0800

    update

---
+++ app/src/main/java/com/martbangunan/app/ui/activity/customer/ProductActivity.kt
@@ -0,0 +1,12 @@
+package com.martbangunan.app.ui.activity.customer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.martbangunan.app.R
+
+class ProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product)
+    }
+}
--- app/src/main/java/com/martbangunan/app/ui/fragment/customer/HomeFragment.kt
+++ app/src/main/java/com/martbangunan/app/ui/fragment/customer/HomeFragment.kt
@@ -1,13 +1,20 @@
 package com.martbangunan.app.ui.fragment.customer
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.button.MaterialButton
 import com.martbangunan.app.R
+import com.martbangunan.app.ui.activity.customer.ProductActivity
 
 class HomeFragment : Fragment() {
+    private val search: TextView by lazy { requireActivity().findViewById(R.id.tvSearch) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +22,14 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        search.setOnClickListener {
+            startActivity(Intent(this.context, ProductActivity::class.java))
+        }
     }
 
 }
